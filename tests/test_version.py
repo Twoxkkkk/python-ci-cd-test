ba20cbfd33a57b1a0556e68d37c2fc23bbd9a340
Igor <sheltov30@gmail.com>
2026-04-16 16:24:33 +0300

    test

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,4 +5,4 @@ def get_version():
     return VERSION
 
 def test_get_version():
-    assert get_version() == 260401 # потенциальная подгрузка с сервера
+    assert get_version() == 260402 # потенциальная подгрузка с сервера
