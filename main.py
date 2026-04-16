03bbc9ee39b3203a3f265a574a7f075f0db159ae
Igor <sheltov30@gmail.com>
2026-04-16 15:56:36 +0300

    project init

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from uvicorn import run
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    run(app, host="0.0.0.0", port=8000)
